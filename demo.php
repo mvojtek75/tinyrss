8f12d005d8d568624be01bdb14da1ab709bd927b
Marian Vojtek <marian.vojtek75@seznam.cz>
2012-12-09 20:39:02 +0100

    First commit

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+  require_once "tinyrss.php";
+
+  tinyRssHeader("My blog", "My blog news", "john@example.com", "http://blog.example.com/");
+
+  $news = explode("\n",file_get_contents("demo.txt"));
+  for ($i=0; $i<count($news); $i++)
+    tinyRssItem("New post", $news[$i], "http://blog.example.com/view?id=$i");
+
+  tinyRssFooter();
+?>
